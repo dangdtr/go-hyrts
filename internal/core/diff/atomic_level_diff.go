ceb0d211357b680ff1e334f33a82ffab38a6fb18
dangdt <dang.dt@teko.vn>
2024-04-22 15:31:11 +0100

    remove pipeline, change var name, pick self test

--- internal/core/diff/atomic_level_diff.go
+++ internal/core/diff/atomic_level_diff.go
@@ -1,23 +1,23 @@
 package diff
 
+import "strings"
+
 func (v *versionDiff) atomicLevelDiff(fileName string) {
 	oldMethMap := v.oldFileMeths[fileName]
 	newMethMap := make(map[string]string)
-
+	pkgName := fileName[:strings.LastIndex(fileName, "/")]
 	for fn, cs := range v.newFileMeths[fileName] {
 		newMethMap[fn] = cs
 	}
-
 	for method, checksum := range oldMethMap {
 		if _, containsKey := newMethMap[method]; !containsKey {
-			v.DFs[fileName] = method
+			v.DMs[pkgName+":"+method] = method
 		} else if newMethMap[method] != checksum {
-			v.CFs[fileName] = method
+			v.CMs[pkgName+":"+method] = method
 		}
 		delete(newMethMap, method)
 	}
 	for method := range newMethMap {
-		v.AFs[fileName] = method
+		v.AMs[pkgName+":"+method] = method
 	}
-
 }
